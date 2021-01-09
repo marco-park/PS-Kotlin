66eef6b4e157de6218eec98c179162e1c89294d8
marco-park <spark328123@gmail.com>
2021-01-10 00:43:09 +0900

    scan setting

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,16 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+private fun readLn() = readLine()!!
+private fun readInt() = readLn().toInt()
+private fun readLong() = readLn().toLong()
+private fun readDouble() = readLn().toDouble()
+private fun readStrings() = readLn().split(" ")
+private fun readInts() = readStrings().map { it.toInt() }
+private fun readLongs() = readStrings().map { it.toLong() }
+private fun readDoubles() = readStrings().map { it.toDouble() }
+
+fun main() {
+    solve()
+}
+
+fun solve() {
+
 }
