65a87804a63a6339fdb496433acd77ddeb9fef2b
marco-park <spark328123@gmail.com>
2021-01-11 18:20:01 +0900

    맵(리팩토링) https://leetcode.com/contest/weekly-contest-222/problems/maximum-units-on-a-truck/

--- src/main/kotlin/leetcode/no1710/Maximum Units on a Truck.kt
+++ src/main/kotlin/leetcode/no1710/Maximum Units on a Truck.kt
@@ -7,16 +7,19 @@ class Solution {
     fun maximumUnits(boxTypes: Array<IntArray>, truckSize: Int): Int {
         var ts = truckSize
 
-        return boxTypes.sortedByDescending { it[1] }
+        return boxTypes.asSequence()
+            .takeWhile { ts != 0 }
+            .sortedByDescending { it[1] }
             .map {
-                val x = (min(ts, it[0])) * it[1]
+                val x = min(ts, it[0]) * it[1]
                 ts = max(0, ts - it[0])
                 x
-            }.sum()
+            }
+            .sum()
     }
 }
 
 fun main() {
     val n = arrayOf(intArrayOf(5,10), intArrayOf(2,5), intArrayOf(4,7), intArrayOf(3,9))
-    print(Solution().maximumUnits(n,10).equals(91))
+    print(Solution().maximumUnits(n,10))
 }
